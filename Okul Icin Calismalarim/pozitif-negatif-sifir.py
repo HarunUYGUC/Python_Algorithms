a0f2660e63faff7f1db1706e239943dca93581dd
Harun Uyguç <harun.uyguc@hotmail.com>
2022-11-25 22:47:25 +0300

    Positive - Negative - Zero

---
+++ Okul Icin Calismalarim/pozitif-negatif-sifir.py
@@ -0,0 +1,32 @@
+x = int(input("Bir sayı giriniz: "))
+
+if (x > 0):
+    print(f"{x} sayısı pozitif bir sayıdır.")
+elif (x == 0):
+    print(f"{x} sayısı 0 (sıfırdır).")
+else:
+    print(f"{x} sayısı negatif bir sayıdır.")
+
+
+# Satır algoritma ile gösterilmesi:
+"""
+A1: Başla.
+A2: Bir sayı giriniz; x
+A3: Eğer x sayısı sıfırdan büyükse "pozitif" yaz.
+A4: Eğer x sayısı sıfırdan küçükse "negatif" yaz.
+A5: Eğer x sayısı sıfıra eşitse "sıfır" yaz.
+A6: Bitir.
+"""
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: Oku x
+A3: Eğer x > 0 ise
+A3.1:   Yaz "Pozitif"
+A4: Eğer x < 0 ise
+A4.1:   Yaz "Negatif"
+A5: Eğer x == 0 ise
+A5.1:   Yaz "Sıfır"
+A6: Bitir
+"""
