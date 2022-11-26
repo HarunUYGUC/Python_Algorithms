83cfc2f648f2f1cdf1b0d2e93200cb1df9ce0e6b
Harun Uyguç <harun.uyguc@hotmail.com>
2022-11-26 18:20:35 +0300

    Addition of Pseudo-code

--- Okul Icin Calismalarim/en-buyuk-sayi.py
+++ Okul Icin Calismalarim/en-buyuk-sayi.py
@@ -32,3 +32,34 @@ else:
 
 print(f"En büyük sayı: {enBuyukSayi}")
 
+
+# Satır algoritma ile gösterilmesi:
+"""
+A1: Başla.
+A2: 1. Sayıyı giriniz.
+A3: 2. Sayıyı giriniz.
+A4: 3. Sayıyı giriniz.
+A5: Eğer 1. sayı hem 2. sayıdan hem de 3. sayıdan büyükse "1. sayı en büyük sayıdır." yaz.
+A6: Eğer 2. sayı hem 1. sayıdan hem de 3. sayıdan büyükse "2. sayı en büyük sayıdır." yaz.
+A7: Eğer 3. sayı hem 1. sayıdan hem de 2. sayıdan büyükse "3. sayı en büyük sayıdır." yaz.
+A8: Bitir.
+"""
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: Yaz "1. Sayıyı (sayi1) giriniz: "
+A3: Oku sayi1
+A4: Yaz "2. Sayıyı (sayi2) giriniz: "
+A5: Oku sayi2
+A6: Yaz "3. Sayıyı (sayi3) giriniz: "
+A7: Oku sayi3
+A8: Eğer (sayi1 > sayi2) ve (sayi1 > sayi3) ise
+A8.1:   enBuyukSayi = sayi1
+A9: Eğer (sayi2 > sayi3) ve (sayi2 > sayi1) ise
+A9.1:   enBuyukSayi = sayi2
+A10: Eğer (sayi3 > sayi1) ve (sayi3 > sayi2) ise
+A10.1:  enBuyukSayi = sayi3
+A11: Yaz enBuyukSayi
+A12: Bitir.
+"""
