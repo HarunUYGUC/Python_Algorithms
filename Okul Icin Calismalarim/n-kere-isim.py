52a75ace793d820888f02e03a6796f774593302f
Harun Uyguç <harun.uyguc@hotmail.com>
2022-11-28 21:01:26 +0300

    n Times Name

---
+++ Okul Icin Calismalarim/n-kere-isim.py
@@ -0,0 +1,22 @@
+isim = input("Bir isim giriniz: ")
+kacKere = int(input("İsminizi kaç kere alt alta yazdırmak istersiniz: "))
+
+i = 1
+while (i <= kacKere):
+    print(f"{i}. {isim}")
+    i += 1
+
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: Yaz "Bir isim (isim) giriniz."
+A3: Oku isim
+A4: Yaz "Kaç kere (kacKere) ismi yazdırmak istersiniz?"
+A5: Oku kacKere
+A6: i = 1
+A7: Döngü (i <= kacKere) ise
+A7.1:   Yaz i + "." + isim
+A7.2:   i = i + 1
+A8: Bitir
+"""
