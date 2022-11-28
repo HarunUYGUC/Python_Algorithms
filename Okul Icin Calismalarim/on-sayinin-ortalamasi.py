f3d9f9b19fea4a3fefe334816947777091e279af
Harun Uyguç <harun.uyguc@hotmail.com>
2022-11-28 21:01:48 +0300

    Sum of Ten Numbers

---
+++ Okul Icin Calismalarim/on-sayinin-ortalamasi.py
@@ -0,0 +1,21 @@
+toplam = 0
+for i in range(1, 11):
+    sayi = float(input(f"{i}. sayıyı giriniz: "))
+    toplam += sayi
+
+ortalama = toplam / 10
+print(f"Sayıların Toplamı = {toplam} | Ortalaması = {ortalama}")
+
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: toplam = 0
+A3: Döngü (i; 1 to 10, adım=1)
+A3.1:   Yaz i + ". sayıyı (sayi) giriniz: "
+A3.2:   Oku sayi
+A3.3:   toplam = toplam + sayi
+A4: ortalama = toplam / 10
+A5: Yaz ortalama
+A6: Bitir
+"""
