7a6d4a4c9c111ae92e7b54fe0265fadb15735bc2
Harun Uyguç <harun.uyguc@hotmail.com>
2022-11-28 23:02:50 +0300

    Squares of Two Numbers

---
+++ Okul Icin Calismalarim/iki-sayinin-kareleri.py
@@ -0,0 +1,18 @@
+sayi = int(input("Pozitif bir tam sayı giriniz: "))
+
+bayrak = False
+i = 0
+j = 0
+
+while (i <= sayi):
+    i += 1
+    j = 0
+    while (j <= sayi):
+        j += 1
+        if (i**2 + j**2 == sayi):
+            print(f"{i}'nin karesi ve {j}'nin karesinin toplamı = {sayi}")
+            bayrak = True
+            break
+
+if (bayrak == False):
+    print(f"{sayi}, iki sayının karesinin toplamı şeklinde yazılamaz.")
