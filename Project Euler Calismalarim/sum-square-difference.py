94ee506af3e9bbf9c9064500efd06d77f65c86bb
Harun Uyguç <harun.uyguc@hotmail.com>
2022-12-15 21:51:59 +0300

    Sum Square Difference

---
+++ Project Euler Calismalarim/sum-square-difference.py
@@ -0,0 +1,11 @@
+sum_of_the_squares = 0
+sum = 0
+
+for i in range(1, 101):
+    sum_of_the_squares += i**2
+    sum += i
+
+square_of_the_sum = sum**2
+result = square_of_the_sum - sum_of_the_squares
+        
+print(f"Difference between the 'sum of the squares' and 'the square of the sum': {result}")
