297ba998768794c0c01726072179728170d257db
Harun Uyguç <harun.uyguc@hotmail.com>
2022-12-17 15:40:57 +0300

    Update

--- Okul Icin Calismalarim/araliktaki-asal-sayilar.py
+++ Okul Icin Calismalarim/araliktaki-asal-sayilar.py
@@ -22,7 +22,7 @@ A6.1:   bayrak = True
 A6.2:   Döngü (j; 2 to (i // 2 + 1), adım=1)
 A6.3:       Eğer (i % j == 0) ise
 A6.4:           bayrak = False
-A6.5:   if (bayrak = True) ise
+A6.5:   Eğer (bayrak == True) ise
 A6.6:       Yaz, "Asal Sayı: " + i
 A7: Bitir
 """
