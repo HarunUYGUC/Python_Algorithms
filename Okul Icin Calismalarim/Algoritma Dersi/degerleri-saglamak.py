8b6953f1bb796ba0ace048d9b89d6bd52213b4fd
Harun Uyguç <harun.uyguc@hotmail.com>
2022-12-22 21:40:36 +0300

    Provide The Values

---
+++ Okul Icin Calismalarim/Algoritma Dersi/degerleri-saglamak.py
@@ -0,0 +1,24 @@
+x = float(input("Bir sayı giriniz: "))
+
+karesi = x**2
+kupu = x**3
+kareKoku = x**0.5
+
+print(f"{x} sayısının karesi: {karesi}")
+print(f"{x} sayısının küpü: {kupu}")
+print(f"{x} sayısının kare kökü: {kareKoku}")
+
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: Yaz, "Bir sayı giriniz: "
+A3: Oku, x
+A4: karesi = x^2
+A5: kupu = x^3
+A6: kareKoku = x^0.5
+A7: Yaz, "Karesi: " + karesi
+A8: Yaz, "Küpü: " + kupu
+A9: Yaz, "Kare Kökü: " + kareKoku
+A10: Bitir
+"""
