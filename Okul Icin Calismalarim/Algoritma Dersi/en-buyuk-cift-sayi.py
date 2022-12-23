86d6a06d909ed43c096512bbafd2f51351beed95
Harun Uyguç <harun.uyguc@hotmail.com>
2022-12-23 21:14:42 +0300

    Greatest Even Number

---
+++ Okul Icin Calismalarim/Algoritma Dersi/en-buyuk-cift-sayi.py
@@ -0,0 +1,31 @@
+kacTane = int(input("Kaç tane pozitif tam sayı gireceksiniz?: "))
+
+enBuyuk = -1
+
+for i in range(1, (kacTane + 1)):
+    sayi = int(input(f"{i}. sayıyı giriniz: "))
+    if (sayi % 2 == 0) and (sayi > enBuyuk):
+        enBuyuk = sayi
+
+if (enBuyuk != -1):
+    print(f"Girilen sayılar arasındaki en büyük çift tam sayısı {enBuyuk} idir.")
+else:
+    print(f"Girilen sayılar arasındaki çift tam sayı yoktur.")
+
+
+# Sözde kod (pseude-code) ile gösterilmesi:
+"""
+A1: Başla
+A2: Yaz, "Kaç tane pozitif tam sayı gireceksiniz?: "
+A3: Oku, kacTane
+A4: enBuyuk = -1
+A5: Döngü (i; 1 to (kacTane + 1), adım = 1)
+A5.1:   Yaz, i + ". sayıyı giriniz: "
+A5.2:   Eğer (sayi % 2 == 0) ve (sayi > enBuyuk) ise
+A5.3:       enBuyuk = sayi
+A6: Eğer (enBuyuk != -1) ise
+A6.1:   Yaz, "En buyuk çift tam sayısı: " + enBuyuk
+A7: Değil ise
+A7.1:   Yaz, "Çift tam sayı yoktur."
+A8: Bitir
+"""
