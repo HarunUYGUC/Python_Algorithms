fd9242f85468f50ba2c872528cd7c897df780b32
Harun Uyguç <harun.uyguc@hotmail.com>
2023-01-02 19:30:53 +0300

    Adding Pseude-Code

--- Okul Icin Calismalarim/Algoritma Dersi/harf-tahmin-oyunu.py
+++ Okul Icin Calismalarim/Algoritma Dersi/harf-tahmin-oyunu.py
@@ -18,3 +18,29 @@ while (i < kacHak):
 
 if (bayrak == False):
     print(f"Bulamadınız! Bulunacak harf {bulunacakHarf} idi.")
+
+
+"""Bu uygulamanın "Sözde Kod"undaki "rastgele harf seçme" önemli. Dikkat et!!!"""
+# "Sözde Kod (pseude-code)" ile gösterilmesi:
+
+"""
+x = random(26) + 65
+bulunacakHarf = ASCIIKar(x)     # ASCII tablosunda büyük harfler 65 - 90 arasında denk geldiği  
+WRITE "Kaç hakta büyük harfi bulmak istersiniz?: " # için böyle bir kod yazılması gereklidir.
+GET kacHak
+i = 0
+flag = False
+LOOP (i < kacHak) THEN
+    WRITE "Tahmininizi giriniz: "
+    GET tahmin
+    i = i + 1
+    IF (tahmin == bulunacakHarf) THEN
+        WRITE "Tebrikler, bulunacak harf " + bulunacakHarf + " idi."
+        flag = True
+        break
+    ENDIF
+ENDLOOP
+IF (flag = False) THEN
+    WRITE "Bulamadınız! Bulunacak harf " + bulunacakHarf + " idi."
+ENDIF
+"""
