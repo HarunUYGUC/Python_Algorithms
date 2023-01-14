9e6eab82b0580d584e4b85a631ea9519d9d83157
Harun Uyguç <harun.uyguc@hotmail.com>
2023-01-14 19:10:54 +0300

    Sum of Squares of Two Numbers

--- Okul Icin Calismalarim/Algoritma Dersi/iki-sayinin-kareleri.py
+++ Okul Icin Calismalarim/Algoritma Dersi/iki-sayinin-kareleri.py
@@ -2,7 +2,6 @@ sayi = int(input("Pozitif bir tam sayı giriniz: "))
 
 bayrak = False
 i = 0
-j = 0
 
 while (i <= sayi):
     i += 1
@@ -19,7 +18,7 @@ if (bayrak == False):
     print(f"{sayi}, iki sayının karesinin toplamı şeklinde yazılamaz.")
 
 
-# Sözde kod (pseude-code) ile gösterilmesi:
+# "Satır Algoritma" ile gösterilmesi:
 """
 A1: Başla
 A2: Yaz, "Pozitif bir tam sayı giriniz: "
@@ -30,14 +29,37 @@ A6: j = 0
 A7: Döngü (i <= sayi) iken
 A7.1:   i = i + 1
 A7.2:   j = 0
-A7.3:       Döngü (j <= sayi) iken
-A7.4:           j = j + 1
-A7.5:           Eğer (i^2 + j^2 == sayi) ise
-A7.6:               Yaz, i + "'nin ve " + j + "'nin karesinin toplamı = " + sayi
-A7.7:               bayrak = True
-A7.8:               i = sayi + 1
-A7.9:               break
+A7.3:   Döngü (j <= sayi) iken
+A7.4:       j = j + 1
+A7.5:       Eğer (i^2 + j^2 == sayi) ise
+A7.6:           Yaz, i + "'nin ve " + j + "'nin karesinin toplamı = " + sayi
+A7.7:           bayrak = True
+A7.8:           i = sayi + 1
+A7.9:           break
 A8: Eğer (bayrak == False) ise
 A8.1:   Yaz, sayi + " iki sayının karesinin toplamı şeklinde yazılamaz."
 A9: Bitir 
+"""
+
+# "Sözde Kod (pseude-code)" ile gösterilmesi:
+"""
+WRITE "Pozitif bir tam sayı giriniz: "
+GET sayi
+bayrak = False
+i = 0
+LOOP (i <= sayi) THEN
+    i = i + 1
+    j = 0
+    LOOP (j <= sayi) THEN
+        j = j + 1
+        IF (i^2 + j^2 == sayi) THEN
+            WRITE i + "'nin ve " + j + "'nin karesinin toplamı = " + sayi
+            bayrak = True
+            i = sayi + 1
+            break
+        ENDIF
+    ENDLOOP
+ENDLOOP
+IF (bayrak == False) THEN
+    WRITE sayi + " iki sayının karesinin toplamı şeklinde yazılamaz."
 """
