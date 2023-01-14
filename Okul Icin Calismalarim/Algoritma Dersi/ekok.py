e04ed2e04d3f44ddea367f12cdae003c257c76dd
Harun Uyguç <harun.uyguc@hotmail.com>
2023-01-14 18:19:48 +0300

    Greatest Common Divisor (GCD)

--- Okul Icin Calismalarim/Algoritma Dersi/ekok.py
+++ Okul Icin Calismalarim/Algoritma Dersi/ekok.py
@@ -20,12 +20,12 @@ for i in asalSayilar:
 print(f"EKOK: {ekok}")
 
 
-# Sözde kod (pseude-code) ile gösterilmesi:
+# "Satır Algoritma" ile gösterilmesi:
 """
 A1: Başla
-A2: Yaz, "1. Sayıyı Giriniz: "
+A2: Yaz, "EKOK'unu bulacağınız 1. sayıyı giriniz: "
 A3: Oku, sayi1
-A4: Yaz, "2. Sayıyı Giriniz: "
+A4: Yaz, "EKOK'unu bulacağınız 2. sayıyı giriniz: "
 A5: Oku, sayi2
 A6: asalSayilar = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
 A7: ekok = 1
@@ -41,6 +41,32 @@ A8.8:           ekok = ekok * i
 A8.9:       Eğer değilse (sayi2 % i == 0) ise
 A8.10:          sayi2 = sayi2 / i
 A8.11:          ekok = ekok * i
-A9: Yaz, ekok
+A9: Yaz, "EKOK: " + ekok
 A10: Bitir
+"""
+
+# "Sözde Kod (pseude-code)" ile gösterilmesi:
+"""
+WRITE "EKOK'unu bulacağınız 1. sayıyı giriniz: "
+GET sayi1
+WRITE "EKOK'unu bulacağınız 2. sayıyı giriniz: "
+GET sayi2
+asalSayilar = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+ekok = 1
+FOR (i in asalSayilar)
+    LOOP (sayi1 % i == 0) or (sayi2 % i == 0) THEN
+        IF (sayi1 % i == 0) and (sayi2 % == 0) THEN
+            sayi1 = sayi1 / i
+            sayi2 = sayi2 / i
+            ekok = ekok * i
+        ELSEIF (sayi1 % i == 0) THEN
+            sayi1 = sayi1 / i
+            ekok = ekok * i
+        ELSE
+            sayi2 = sayi2 / i
+            ekok = ekok * i
+        ENDIF
+    ENDLOOP
+ENDFOR
+WRITE "EKOK: " + ekok
 """
