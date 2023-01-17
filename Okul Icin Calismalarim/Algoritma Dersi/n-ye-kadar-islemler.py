d96f3ad49d3ec81483753b271710cdb554959ef0
Harun Uyguç <harun.uyguc@hotmail.com>
2023-01-17 15:07:03 +0300

    Transactions Up to n

--- Okul Icin Calismalarim/Algoritma Dersi/n-ye-kadar-islemler.py
+++ Okul Icin Calismalarim/Algoritma Dersi/n-ye-kadar-islemler.py
@@ -4,7 +4,7 @@ tekToplami = 0
 tekCarpimi = 1
 ciftlerinKareleriToplami = 0
 
-for i in range(1, x+1):
+for i in range(1, (x + 1)):
     if (i % 2 == 0):
         ciftinKaresi = i**2
         ciftlerinKareleriToplami += ciftinKaresi
@@ -12,5 +12,26 @@ for i in range(1, x+1):
         tekToplami += i
         tekCarpimi *= i
 
-print(f"Teklerin Toplamı: {tekToplami} \nTeklerin Çarpımı: {tekCarpimi}")
-print(f"Çiflerin Karelerinin Toplamı: {ciftlerinKareleriToplami}")
+print(f"{x}'e Kadar Ki Teklerin Toplamı: {tekToplami} \nTeklerin Çarpımı: {tekCarpimi}")
+print(f"{x}'e Kadar Ki Çiflerin Karelerinin Toplamı: {ciftlerinKareleriToplami}")
+
+
+# "Sözde Kod (pseude-code)" ile gösterilmesi:
+"""
+WRITE "Bir sayı giriniz: "
+GET sayi
+tekToplami = 0
+tekCarpimi = 1
+ciftlerinKareleriToplami = 0
+FOR (i; 1 to (sayi + 1), STEP = 1)
+    IF (i % 2 == 0) THEN
+        ciftinKaresi = i^2
+        ciftlerinKareleriToplami = ciftlerinKareleriToplami + ciftinKaresi
+    ELSE
+        tekToplami = tekToplami + i
+        tekCarpimi = tekCarpimi * i
+    ENDIF
+WRITE sayi + "'e Kadar Ki Teklerin Toplamı: " + tekToplami
+WRITE sayi + "'e Kadar Ki Teklerin Çarpımı: " + tekCarpimi
+WRITE sayi + "'e Kadar Ki Çiftlerin Kareleri Toplamı: " + ciftlerinKareleriToplami
+"""
