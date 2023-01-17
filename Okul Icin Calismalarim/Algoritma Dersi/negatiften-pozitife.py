bcd504ead8e698eb4b620a58933481b1aedb6917
Harun Uyguç <harun.uyguc@hotmail.com>
2023-01-17 15:23:10 +0300

    Negative to Positive

--- Okul Icin Calismalarim/Algoritma Dersi/negatiften-pozitife.py
+++ Okul Icin Calismalarim/Algoritma Dersi/negatiften-pozitife.py
@@ -6,13 +6,23 @@ if (sayi < 0):
 print(f"Sayının pozitif hali = {sayi}")
 
 
-# Sözde kod (pseude-code) ile gösterilmesi:
+# "Satır Algoritma" ile gösterilmesi:
 """
 A1: Başla
 A2: Yaz, "Bir sayı giriniz: "
 A3: Oku, sayi
 A4: Eğer (sayi < 0) ise
 A5:     sayi = sayi * (-1)
-A6: Yaz, sayi
+A6: Yaz, "Sayının pozitif hali: " + sayi
 A7: Bitir
+"""
+
+# "Sözde Kod (pseude-code)" ile gösterilmesi:
+"""
+WRITE "Bir sayı giriniz: "
+GET sayi
+IF (sayi < 0) THEN
+    sayi = sayi * (-1)
+ENDIF
+WRITE "Sayının pozitif hali: " + sayi
 """
