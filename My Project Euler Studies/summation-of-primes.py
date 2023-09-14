63fb22633eae95ca0d766b0836684ac69489c8a1
Harun Uyguç <harun.uyguc@hotmail.com>
2023-09-14 21:12:27 +0300

    Correction

--- My Project Euler Studies/summation-of-primes.py
+++ My Project Euler Studies/summation-of-primes.py
@@ -3,20 +3,20 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
 
-# sum = 0
+sum = 0
 
-# for i in range(2, 10000): # If I add another zero, it's very slow after that.
-#     flag = True
+for i in range(2, 10): # It's very slow after 10000.
+    flag = True
 
-#     for j in range(2, i//2 + 1):
-#         if (i % j == 0):
-#             flag = False
-#             break
+    for j in range(2, i//2 + 1):
+        if (i % j == 0):
+            flag = False
+            break
     
-#     if (flag == True):
-#         sum += i
+    if (flag == True):
+        sum += i
 
-# print(f"Sum: {sum}")
+print(f"Sum: {sum}")
 
 
 """ My code runs very slowly. So I used the following code I found online. 
