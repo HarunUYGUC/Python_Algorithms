6beec7f210dd6d07cd49eeb02cb1d3a72bacf544
Harun Uyguç <harun.uyguc@hotmail.com>
2023-09-16 18:00:46 +0300

    1000 Digit Fibonacci Number

---
+++ My Project Euler Studies/1000-digit-fibonacci-number.py
@@ -0,0 +1,41 @@
+"""
+The Fibonacci sequence is defined by the recurrence relation: 
+F(n) = F(n-1) + F(n - 2), where F1 = 1 and F2 = 1.
+
+Hence the first 12 terms will be:
+    F1 = 1
+    F2 = 1
+    F3 = 2
+    F4 = 3
+    F5 = 5
+    F6 = 8
+    F7 = 13
+    F8 = 21
+    F9 = 34
+    F10 = 55
+    F11 = 89
+    F12 = 144
+
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence 
+to contain 1000 digits?
+"""
+
+number1 = 1
+number2 = 1
+list = [number1, number2]
+index = len(list) - 1
+
+while True:
+    sum = number1 + number2
+    list.append(sum)
+
+    number1 = number2
+    number2 = sum
+
+    index += 1
+
+    if (len(str(list[index])) == 1000):
+        print(index + 1)
+        break
