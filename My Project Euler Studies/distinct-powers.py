471ae59bcfb5d54b09455b09b35a9389f7270dc5
Harun Uyguç <harun.uyguc@hotmail.com>
2023-09-20 16:08:35 +0300

    Update

--- My Project Euler Studies/distinct-powers.py
+++ My Project Euler Studies/distinct-powers.py
@@ -6,7 +6,7 @@ for a in range(2, 101):
     for b in range(2, 101):
         expo = a**b
 
-        if (expo not in list):
+        if expo not in list:
             list.append(expo)
 
 print(len(list))
