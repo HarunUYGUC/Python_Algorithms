44a77dfb9c133f808a4b83b58d033cdedf9cd63f
Harun Uyguç <harun.uyguc@hotmail.com>
2023-09-20 16:06:53 +0300

    Distinct Powers

---
+++ My Project Euler Studies/distinct-powers.py
@@ -0,0 +1,12 @@
+""" https://projecteuler.net/problem=29 """
+
+list = []
+
+for a in range(2, 101):
+    for b in range(2, 101):
+        expo = a**b
+
+        if (expo not in list):
+            list.append(expo)
+
+print(len(list))
