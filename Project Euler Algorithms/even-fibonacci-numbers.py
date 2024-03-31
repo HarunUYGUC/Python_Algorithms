61f7d714a32854ce9e35b55e42b1733c4b882a99
Harun Uyguç <harun.uyguc@hotmail.com>
2024-03-31 11:51:37 +0300

    Method 2

--- Project Euler Algorithms/even-fibonacci-numbers.py
+++ Project Euler Algorithms/even-fibonacci-numbers.py
@@ -1,5 +1,7 @@
 """ https://projecteuler.net/problem=2 """
 
+# Method 1 
+
 sayi1 = 0
 sayi2 = 1
 liste = [sayi1, sayi2]
@@ -19,3 +21,19 @@ while True:
 
 print(f"4.000.000'den küçük fibonacci sayıları {liste} idir.")
 print(f"4.000.000'dan küçük çift fibonacci sayılarının toplamı: {ciftFibonacciToplamlari}")
+
+
+# Method 2
+
+a = 1
+b = 2
+sum = b # sum of the even-valued terms
+
+while (b <= 4000000):
+    a, b = b, a
+    b = a + b
+
+    if (b % 2 == 0):
+        sum += b
+
+print(sum)
