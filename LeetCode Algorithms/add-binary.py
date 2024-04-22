88d8f165b176c9e9cb6ce899863021281276d2f0
Harun Uyguç <harun.uyguc@hotmail.com>
2024-04-22 22:28:30 +0300

    Add Binary

---
+++ LeetCode Algorithms/add-binary.py
@@ -0,0 +1,44 @@
+""" https://leetcode.com/problems/add-binary/description/ """
+
+class Solution:
+    def addBinary(self, a, b):
+        # a = "0" and b = "0"
+        if (a == "0") and (b == "0"):
+            return "0"
+
+        # a
+        suma = 0
+        lena = len(a) - 1
+
+        for i in range(len(a)):
+            if (int(a[i]) == 1):
+                suma += 2**lena
+
+            lena -= 1
+
+        # b
+        sumb = 0
+        lenb = len(b) - 1
+
+        for i in range(len(b)):
+            if (int(b[i]) == 1):
+                sumb += 2**lenb
+                
+            lenb -= 1
+        
+        # total
+        total = suma + sumb
+        binary = ""
+        newBinary = ""
+
+        while (total > 0):
+            binary += str(total % 2)
+            total //= 2
+        
+        for i in range(len(binary) - 1, -1, -1):
+            newBinary += binary[i]
+
+        return newBinary
+    
+solution = Solution()
+print(solution.addBinary("1010", "1011"))
