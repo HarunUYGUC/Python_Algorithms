cbe803a1eb071a939565c10d7558b5bb1b8846da
Harun Uyguç <harun.uyguc@hotmail.com>
2024-04-22 22:35:49 +0300

    Not Finished Yet

---
+++ LeetCode Algorithms/valid-parentheses.py
@@ -0,0 +1,21 @@
+""" https://leetcode.com/problems/valid-parentheses/ """
+
+class Solution:
+    def isValid(self, s):
+        flag = True
+
+        for i in range(0, len(s) - 1, 2):
+            if (s[i] == "(") and (s[i + 1] == ")"):
+                pass
+            elif (s[i] == "[") and (s[i + 1] == "]"):
+                pass
+            elif (s[i] == "{") and (s[i + 1] == "}"):
+                pass
+            else:
+                return False
+        
+        if (flag == True):
+            return True
+
+solution = Solution()
+print(solution.isValid("()[]{}"))
