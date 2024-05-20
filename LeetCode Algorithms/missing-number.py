e0cef4da3ee649983d8dfab86d57cf27e47ce056
Harun Uyguç <harun.uyguc@hotmail.com>
2024-05-20 11:25:57 +0300

    Missing Number

--- LeetCode Algorithms/missing-number.py
+++ LeetCode Algorithms/missing-number.py
@@ -1,5 +1,7 @@
 """ https://leetcode.com/problems/missing-number/ """
 
+# Method 1
+
 class Solution:
     def missingNumber(self, nums):
         list = []
@@ -10,6 +12,31 @@ class Solution:
         for j in list:
             if j not in nums:
                 return j
+
+solution = Solution()
+print(solution.missingNumber([9,6,4,2,3,5,7,0,1]))
+
+
+# Method 2
+
+class Solution:
+    def missingNumber(self, nums):
+        list = []
+
+        for i in range(len(nums) + 1):
+            list.append(i)
+
+        for i in range(len(list)):
+            flag = False
+
+            for j in range(len(nums)):
+                if (list[i] == nums[j]):
+                    nums.pop(j)
+                    flag = True
+                    break
+
+            if (flag == False):
+                return list[i]
 
 solution = Solution()
 print(solution.missingNumber([9,6,4,2,3,5,7,0,1]))
