ee65d9af5cfa7ab148a7e6bba764b2646cae2e65
Harun Uyguç <harun.uyguc@hotmail.com>
2024-08-26 12:01:22 +0300

    Not Completed

---
+++ LeetCode Algorithms/toeplitz-matrix.py
@@ -0,0 +1,24 @@
+""" https://leetcode.com/problems/toeplitz-matrix/ """
+
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        if (len(matrix) > len(matrix[0])):
+            length = len(matrix[0])
+        elif (len(matrix) < len(matrix[0])):
+            length = len(matrix)
+        else:
+            length = len(matrix)
+
+        numbers = []
+
+        for i in range(length):
+            numbers.append(matrix[i][i])
+        
+        piece = numbers.count(numbers[0])
+
+        if (piece == length):
+            return True
+        else:
+            return False
+        
+print(Solution().isToeplitzMatrix([[11,74,0,93],[40,11,74,7]]))
