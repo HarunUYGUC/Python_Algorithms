d6c90f25f04d2fc5f14a6be11758dfcf6fb38bf0
Harun Uyguç <harun.uyguc@hotmail.com>
2024-08-27 11:21:40 +0300

    Find Pivot Index

--- LeetCode Algorithms/find-pivot-index.py
+++ LeetCode Algorithms/find-pivot-index.py
@@ -1,41 +1,33 @@
 """ https://leetcode.com/problems/find-pivot-index/ """
 
-# 739 / 746 testcases passed
-
 class Solution:
     def pivotIndex(self, nums):
-        for i in range(len(nums)):
-            if (i == 0):
-                leftSum = 0
-                rightSum = 0
+        sumAll = 0
 
-                for j in range(1, len(nums)):
-                    rightSum += nums[j]
+        for num in nums:
+            sumAll += num
+
+        for i in range(len(nums)):            
+            if (i > 0) and (i < len(nums) - 1):
+                rightSum -= nums[i]
+                
+                leftSum += nums[i - 1]
+
+                if (leftSum == rightSum):
+                    return i
+            elif (i == 0):
+                leftSum = 0
+                rightSum = sumAll - nums[i]
                 
                 if (leftSum == rightSum):
                     return i
             elif (i == len(nums) - 1):
-                leftSum = 0
+                leftSum = sumAll - nums[i]
                 rightSum = 0
-
-                for j in range(len(nums) - 2, -1, -1):
-                    leftSum += nums[j]
 
                 if (leftSum == rightSum):
                     return i
-            else:
-                leftSum = 0
-                rightSum = 0
-
-                for j in range(i + 1, len(nums)):
-                    rightSum += nums[j]
                 
-                for j in range(i - 1, -1, -1):
-                    leftSum += nums[j]
-
-                if (leftSum == rightSum):
-                    return i
-
         return -1
     
-print(Solution().pivotIndex([2,1,-1]))
+print(Solution().pivotIndex([1,7,3,6,5,6]))
