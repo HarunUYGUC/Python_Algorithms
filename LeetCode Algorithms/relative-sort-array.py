c3579e7d880013c6ca1ccc6d5ac54496b557e5dd
Harun Uyguç <harun.uyguc@hotmail.com>
2024-08-29 22:44:11 +0300

    Relative Sort Array

--- LeetCode Algorithms/relative-sort-array.py
+++ LeetCode Algorithms/relative-sort-array.py
@@ -2,23 +2,19 @@
 
 class Solution:
     def relativeSortArray(self, arr1, arr2):
-        list1 = []
-        list2 = []
+        list = []
 
         for arr in arr2:
             if arr in arr1:
                 total = arr1.count(arr)
 
                 while (total != 0):
-                    list1.append(arr)
+                    list.append(arr)
                     arr1.remove(arr)
                     total -= 1
-            else:
-                list2.append(arr)
             
-        list1.extend(list2)
         arr1.sort()
-        list1.extend(arr1)
-        return list1
+        list.extend(arr1)
+        return list
     
 print(Solution().relativeSortArray([2,3,1,3,2,4,6,7,9,2,19], [2,1,4,3,9,6]))
