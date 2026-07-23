44180ef010e0acf5865580e05a7ef9a51d1c42ad
Harun Uyguç <harun.uyguc@hotmail.com>
2026-07-23 18:26:38 +0300

    Find Minimum in Rotated Sorted Array

---
+++ NeetCode/Medium/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMin(self, nums):
+        left = 0
+        right = len(nums) - 1
+
+        while (left < right):
+            mid = (left + right) // 2
+
+            if (nums[mid] > nums[right]):
+                left = mid + 1
+            else:
+                right = mid
+            
+        return nums[right]
